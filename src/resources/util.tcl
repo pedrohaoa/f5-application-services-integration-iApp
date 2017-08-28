06d2841ba497c2f4e668c2982286d0b44fe3251b
Rafal Korepta <rafal.korepta@gmail.com>
2017-08-27 21:10:09 -0700

    Test 100 iteration.

--- src/resources/util.tcl
+++ src/resources/util.tcl
@@ -401,10 +401,10 @@ proc process_options_string { option_str tmsh template {add_default 0} } {
           set profileobj [lindex [tmsh::get_config ltm $tmsh $template all-properties] 0]
           set is_valid_profile [is_valid_profile_option $profileobj $option]
         }]} { set is_valid_profile 0 }
-        if {$is_valid_profile != 0 || $counter == 10 } break
+        if {$is_valid_profile != 0 || $counter == 100 } break
         set counter [expr {$counter + 1}]
       }
-      if {$counter == 10} {
+      if {$counter == 100} {
         error "There is problem with mcpd"
       }
       if {[catch {
